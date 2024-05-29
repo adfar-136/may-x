38c29e450b9af9ff235f78cd23972792d5450a4d
adfar-136 <adfarrasheed136@gmail.com>
2024-05-29 22:29:36 +0530

    array functions

--- Javascript/array.js
+++ Javascript/array.js
@@ -2,8 +2,69 @@
 // console.log(arr[7])
 // var arr = [[2,4,1],[3,6,9],[2,3,5]]
 // console.log(arr[2][0])
-var arr =[2,3,4,5,6]
 // arr.push("a","b",5,6,7)
 // arr.pop()
-console.log(arr.pop())
-console.log(arr)
+// console.log(arr.pop())
+// console.log(arr)
+// var arr2 = arr.slice(0,2)
+// console.log(arr)
+
+// var arr =[2,3,4,5,6,7,8,9]
+// var arr2 = arr.splice(4,0,"a","b","c","adfar")
+// console.log(arr)
+
+// var sum = 0;
+// for(let i=0;i<arr.length;i++){
+//     sum = sum + arr[i];
+// }
+// console.log(sum)
+// var arr = [2,3,4,5,6,7,8,9];
+// var sum=0;
+// for(let element of arr){
+//     sum=sum+element
+// }
+// console.log(sum)
+
+var arr = [3,6,9,1,2,5];
+var arr2 =arr.map((x,y)=>{
+    return x*4
+})
+// var arr2 =arr.map(function(x,y){
+//     return x*4
+// })
+// console.log(arr2)
+
+// var arr = [3,6,9,1,2,5];
+// var arr2 =arr.filter(function(x,y){
+//     return x*4
+// })
+// console.log(arr2)
+
+// var arr = [3,14.5,86.5,-500,340,32,61]
+// arr.sort(function(a,b){
+//    return a-b
+// })
+// console.log(arr)
+
+// var arr= [2,3,4,3,2,4]
+// var sum= arr.reduce(function(total,item,index){
+//     return total+item
+// },10)
+// console.log(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/functions.js
@@ -0,0 +1,30 @@
+// function greet(){
+//     return "adfar"
+// }
+// console.log(greet());
+// var sayMyName = function(){
+//     console.log("Adfar Rashid")
+// }
+// function greet(firstName,secondName){
+//    console.log(firstName + " " + secondName)
+// }
+// greet("Adfar","Rashid")
+
+// function greet(x,y){
+//     x()
+//     console.log(y)
+// }
+// function sayMyName(){
+//     console.log("Adfar Rashid")
+// }
+// greet(sayMyName,4)
+
+
+// var greet = function(x){
+//     return x
+// }
+var greet = (x)=>{
+    return x
+}
+
+console.log(greet(4))
