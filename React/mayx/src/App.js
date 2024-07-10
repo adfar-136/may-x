910e3953c50097ac32d4846fcce8cc3947a39de3
adfar-136 <adfarrasheed136@gmail.com>
2024-07-10 22:58:25 +0530

    state completed

--- React/mayx/src/App.js
+++ React/mayx/src/App.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import Counter from './components/Counter';
+import React, { useState } from 'react'
 
 export default function App() {
-  let count = 0;
-  function handleAdd(){
-    console.log("adfar")
-    count++;
-  }
+  const [input,setInput] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
   return (
     <div>
-      <Counter/>
-      <h1>Count: {count}</h1>
-      <button onClick={handleAdd}>Add One</button>
+      <form>
+        <input type="text" placeholder='Enter Your Name'
+        value={input.name} 
+        onChange={(e)=>setInput((prev)=>({...prev,name:e.target.value}))}/>
+        <br /><br />
+        <input type="email" placeholder='Enter your Email' 
+        value={input.email} 
+        onChange={(e)=>setInput((prev)=>({...prev,email:e.target.value}))}/>
+        <br /><br />
+        <input type="password" placeholder='Enter Your Password'
+        value={input.password} 
+        onChange={(e)=>setInput((prev)=>({...prev,password:e.target.value}))}/>
+        <br /><br />
+        <input type="submit" value="Submit" />
+      </form>
+     
+        <div>
+
+        <h1>My Name is {input.name}</h1>
+        <h1>My Email is {input.email}</h1>
+        <h1>My Password is {input.password}</h1>
+      </div>
     </div>
   )
 }
