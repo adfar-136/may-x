c4b35ed3742d0c8fb2c3b5ee091f4a2686d18ff5
adfar-136 <adfarrasheed136@gmail.com>
2024-07-12 22:58:10 +0530

    useEffect

--- React/mayx/src/App.js
+++ React/mayx/src/App.js
@@ -1,35 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
-  const [input,setInput] = useState({
-    name:"",
-    email:"",
-    password:"",
-  })
+  const [count,setCount] =useState(0)
+ 
+  useEffect(()=>{
+    function fun(){
+      console.log("Adfar")
+    }
+    document.addEventListener("click",fun)
+    return ()=>{
+      document.removeEventListener("click",fun)
+    }
+  }) 
   return (
     <div>
-      <form>
-        <input type="text" placeholder='Enter Your Name'
-        value={input.name} 
-        onChange={(e)=>setInput((prev)=>({...prev,name:e.target.value}))}/>
-        <br /><br />
-        <input type="email" placeholder='Enter your Email' 
-        value={input.email} 
-        onChange={(e)=>setInput((prev)=>({...prev,email:e.target.value}))}/>
-        <br /><br />
-        <input type="password" placeholder='Enter Your Password'
-        value={input.password} 
-        onChange={(e)=>setInput((prev)=>({...prev,password:e.target.value}))}/>
-        <br /><br />
-        <input type="submit" value="Submit" />
-      </form>
-     
-        <div>
-
-        <h1>My Name is {input.name}</h1>
-        <h1>My Email is {input.email}</h1>
-        <h1>My Password is {input.password}</h1>
-      </div>
+      <h1>React App</h1>
+      <h1>count:{count}</h1>
+      <button onClick={()=>setCount(count+1)}>+</button>
+      
     </div>
   )
 }
